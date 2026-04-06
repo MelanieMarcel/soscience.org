34e192b973cb44a21ef9b08c9b5bc2c1a2ea0fb7
Melanie Marcel <melanie.marcel@proton.me>
2026-04-06 22:19:17 +0200

    Add _redirects file to catch all paths and redirect to homepage

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/_redirects": "_redirects" });
 
   return {
     dir: {
